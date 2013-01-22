1fdf528afb57953cc49987bfa781fe5995b84d08
eernie <oldenkamperwin@gmail.com>
2013-01-22 15:27:32 +0100

    fix serializer

--- toekomstwinkel/settings.py
+++ toekomstwinkel/settings.py
@@ -127,6 +127,10 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+SERIALIZATION_MODULES = {
+    'json': 'wadofstuff.django.serializers.json'
+}
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
